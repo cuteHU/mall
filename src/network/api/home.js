1da33eca2aad793d2203813f91ad80f189dbde2a
cuteHU <541707796@qq.com>
2019-12-27 16:49:43 +0800

    fix

--- src/network/api/home.js
+++ src/network/api/home.js
@@ -8,3 +8,13 @@ export function getHomeMultidata() {
         method: 'get'
     })
 }
+
+export function getHomeGoods(type, page) {
+    return request({
+        url: '/home/data',
+        params: {
+            type,
+            page
+        }
+    })
+}
