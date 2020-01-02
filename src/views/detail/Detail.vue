374d207437e2e33d1d969d3400924a38b6e5798f
cuteHU <541707796@qq.com>
2020-01-02 14:28:44 +0800

    fix

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,40 +1,105 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :topImages="topImages"></detail-swiper>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll ref="scroll"
+            class="content">
+      <detail-swiper :topImages="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo"
+                         @imageLoad="imageLoad"></detail-goods-info>
+      <detail-param-info :param-info="paramInfo"></detail-param-info>
+      <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
+    </scroll>
   </div>
 </template>
 
 <script>
-import { getDetail } from 'network/api/detail'
+import { getDetail, Goods, Shop, GoodsParam } from 'network/api/detail'
+
+import Scroll from 'components/common/scroll/Scroll'
 
 import DetailNavBar from './childComponents/DetailNavBar'
 import DetailSwiper from './childComponents/DetailSwiper'
+import DetailBaseInfo from './childComponents/DetailBaseInfo'
+import DetailShopInfo from './childComponents/DetailShopInfo'
+import DetailGoodsInfo from './childComponents/DetailGoodsInfo'
+import DetailParamInfo from './childComponents/DetailParamInfo'
+import DetailCommentInfo from './childComponents/DetailCommentInfo'
 
 export default {
   name: 'Detail',
   components: {
+    Scroll,
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    DetailCommentInfo
   },
   data () {
     return {
       iid: null,
-      topImages: []
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
+      commentInfo: {},
     }
   },
   created () {
     this.iid = this.$route.params.id
     getDetail(this.iid).then((result) => {
-      // console.log(result);
+      const data = result.result
+      console.log(result.result);
       // 1.获取顶部的图片轮播数据
-      this.topImages = result.result.itemInfo.topImages
+      this.topImages = data.itemInfo.topImages
+
+      // 2.获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+
+      // 3.创建店铺信息
+      this.shop = new Shop(data.shopInfo)
+
+      // 4.保存商品的详情数据
+      this.detailInfo = data.detailInfo
+
+      // 5.获取参数的信息
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+
+      // 6.取出评论信息
+      if (data.rate.cRate != 0) {
+        this.commentInfo = data.rate.list[0]
+      }
     }).catch((err) => {
 
     });
   },
+  methods: {
+    imageLoad () {
+      this.$refs.Scroll.refresh()
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
+#detail {
+  position: relative;
+  height: 100vh;
+  background-color: #fff;
+  overflow: hidden;
+  z-index: 9;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+.content {
+  height: calc(100% - 44px);
+}
 </style>
